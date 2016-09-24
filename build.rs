5126677c03ca780fd8b68fd9c2f0a09794389d49
Nathan Vegdahl <cessen@cessen.com>
2016-09-23 23:37:29 -0700

    Only build static libraries.

--- build.rs
+++ build.rs
@@ -2,8 +2,11 @@ extern crate cmake;
 
 fn main() {
     // Build IlmBase
-    let ilmbase_dst = cmake::build("openexr/IlmBase");
+    let ilmbase_dst = cmake::Config::new("openexr/IlmBase")
+        .define("BUILD_SHARED_LIBS", "OFF")
+        .build();
     let _ = cmake::Config::new("openexr/OpenEXR")
         .define("ILMBASE_PACKAGE_PREFIX", &ilmbase_dst)
+        .define("BUILD_SHARED_LIBS", "OFF")
         .build();
 }
