4fb459dd375b669540f53a283ab8f6632f8e6285
Benjamin Saunders <ben.e.saunders@gmail.com>
2017-05-13 19:50:59 -0700

    Fix windows build

--- openexr-sys/build.rs
+++ openexr-sys/build.rs
@@ -20,10 +20,12 @@ fn main() {
         println!("cargo:rustc-link-search=native={}", ilmbase_dst.join("lib").display());
 
         // Build OpenEXR
-        let openexr_dst = cmake::Config::new("openexr/OpenEXR")
-            .define("ILMBASE_PACKAGE_PREFIX", &ilmbase_dst)
-            .define("BUILD_SHARED_LIBS", "OFF")
-            .build();
+        let mut openexr_cfg = cmake::Config::new("openexr/OpenEXR");
+        openexr_cfg.define("ILMBASE_PACKAGE_PREFIX", &ilmbase_dst)
+            .define("BUILD_SHARED_LIBS", "OFF");
+        #[cfg(windows)]
+        openexr_cfg.cxxflag("/DWIN32");
+        let openexr_dst = openexr_cfg.build();
         println!("cargo:rustc-link-search=native={}", openexr_dst.join("lib").display());
 
         // Link all the libs from OpenEXR
@@ -41,8 +43,11 @@ fn main() {
     // Build C wrapper for OpenEXR
     let mut gcc = gcc::Config::new();
     gcc.cpp(true)
-        .flag("-std=c++11")
         .include("c_wrapper");
+    #[cfg(target_env = "msvc")]
+    gcc.flag("/std:c++14");
+    #[cfg(not(target_env = "msvc"))]
+    gcc.flag("-std=c++14");
     for path in &include_paths {
         gcc.include(path);
     }
@@ -51,5 +56,12 @@ fn main() {
         .compile("libcexr.a");
 
     // Find and link zlib, needed for OpenEXR
-    pkg_config::probe_library("zlib").unwrap();
+    if let Err(err) = pkg_config::probe_library("zlib") {
+        if let Ok(path) = env::var("ZLIB_LIBRARY") {
+            println!("cargo:rustc-link-search=native={}", path);
+            println!("cargo:rustc-link-lib=static=zlibstatic");
+        } else {
+            panic!("couldn't find zlib: ZLIB_LIBRARY is unset and pkg-config failed: {}", err);
+        }
+    }
 }
