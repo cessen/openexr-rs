9a6bc674caed9285855cc7eaf2a2c1d3adbe1b1b
Nicola Orru <nigu.orru@gmail.com>
2018-11-19 22:51:20 +0000

    Better comments in the new example

--- tests/non_zero_origin_window.rs
+++ tests/non_zero_origin_window.rs
@@ -18,31 +18,31 @@ fn load_and_test_with_offset_window_read_multiple_channels(data: &[u8]) {
 
     let zero = f16::from_f32(0.0f32);
 
-    println!("Reading pixels from {},{},{}", "R", "G", "B");
     let mut pixel_data = vec![(zero, zero, zero); (width * height) as usize];
 
-    // let's try a mismatched origin 0,0
     let read_with_offset = |exr_file: &mut InputFile, pixel_data: &mut [_], ox, oy| {
         let mut fb = FrameBufferMut::new_with_origin(ox, oy, width, height);
         println!("Loading from buffer as {},{} {}x{}", ox, oy, width, height);
         fb.insert_channels(&[("R", 0.0), ("G", 0.0), ("B", 0.0)], pixel_data);
-        exr_file.read_pixels(&mut fb)
+        println!("Reading pixels from {},{},{}", "R", "G", "B");
+        (exr_file.read_pixels(&mut fb), fb.origin_offset())
     };
 
-    assert!(read_with_offset(&mut exr_file, &mut pixel_data, 0, 0).is_err());
-    assert!(read_with_offset(&mut exr_file, &mut pixel_data, x - 1, y - 1).is_err());
-    assert!(read_with_offset(&mut exr_file, &mut pixel_data, -x, -y).is_err());
-    assert!(read_with_offset(&mut exr_file, &mut pixel_data, x, y).is_ok());
-
+    // let's try a few mismatched origins
+    assert!(read_with_offset(&mut exr_file, &mut pixel_data, 0, 0)
+        .0
+        .is_err());
+    assert!(
+        read_with_offset(&mut exr_file, &mut pixel_data, x - 1, y - 1)
+            .0
+            .is_err()
+    );
+    assert!(read_with_offset(&mut exr_file, &mut pixel_data, -x, -y)
+        .0
+        .is_err());
     // and then the real thing
-    let origin_offset = {
-        let mut fb = FrameBufferMut::new_with_origin(x, y, width, height);
-        println!("Loading from buffer as {}x{}", width, height);
-        fb.insert_channels(&[("R", 0.0), ("G", 0.0), ("B", 0.0)], &mut pixel_data);
-        exr_file.read_pixels(&mut fb).unwrap();
-        fb.origin_offset()
-    };
-
+    let (read_result, origin_offset) = read_with_offset(&mut exr_file, &mut pixel_data, x, y);
+    assert!(read_result.is_ok());
     // check the pixel value at coordinates (0,0) of the data window if 0,0 is within the frame buffer
     if origin_offset >= 0 {
         assert!(f32::abs(pixel_data[origin_offset as usize].0.to_f32() - 0.5f32) < 0.0001f32);
@@ -77,10 +77,8 @@ fn load_and_test_with_offset_window_read_single_channel(data: &[u8]) {
 
     let zero = f16::from_f32(0.0f32);
 
-    println!("Reading pixels from {},{},{}", "R", "G", "B");
     let mut pixel_data = vec![zero; (width * height) as usize];
 
-    // let's try a mismatched origin near the origin
     let mut read_with_offset = |ox: i32, oy: i32| {
         let mut fb = FrameBufferMut::new_with_origin(ox, oy, width, height);
         println!(
@@ -88,12 +86,14 @@ fn load_and_test_with_offset_window_read_single_channel(data: &[u8]) {
             ox, oy, width, height
         );
         fb.insert_channel("R", 0.0, &mut pixel_data);
+        println!("Reading pixels from {},{},{}", "R", "G", "B");
         exr_file.read_pixels(&mut fb)
     };
-
+    // let's try a few mismatched origins
     assert!(read_with_offset(0, 0).is_err());
     assert!(read_with_offset(x - 1, y - 1).is_err());
     assert!(read_with_offset(-x, -y).is_err());
+    // and then the correct one
     assert!(read_with_offset(x, y).is_ok());
 }
 
