ece46c46f3875be80347c5d01bdcb3bfcd02955d
Benjamin Saunders <ben.e.saunders@gmail.com>
2019-05-08 17:17:43 -0700

    Regenerate bindings

--- bindgen.sh
+++ bindgen.sh
@@ -4,4 +4,5 @@ bindgen \
     --whitelist-function ".*CEXR.*" \
     --whitelist-type ".*CEXR.*" \
     --whitelist-var ".*CEXR.*" \
+    --rustified-enum "CEXR_.*" \
     openexr-sys/c_wrapper/cexr.h > openexr-sys/src/bindings.rs
